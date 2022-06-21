1b1ac347b4b569f0a5d755a3be89ac416ad7a0ba
Lucas Brito <lucasbsilva29@gmail.com>
2022-06-20 23:36:46 -0300

    feat(images_converter.py): making an algorithmn to convert images format

---
+++ images_converter.py
@@ -0,0 +1,40 @@
+import io
+import glob
+import pyheif
+import whatimage
+from PIL import Image
+
+
+def convert_image(image: str = None, output_format: str = 'tiff', output_path: str = None):
+    img_name = image.split('/')[-1].split('.')[0]
+    image = read_img(image)
+    img_format = whatimage.identify_image(image)
+    if img_format in ['heic', 'avif']:
+        img = pyheif.read_heif(image)
+        data = Image.frombytes(
+            mode=img.mode,
+            size=img.size,
+            data=img.data,
+            decoder_name='raw'
+        )
+    else:
+        data = Image.open(io.BytesIO(image))
+
+    data.save(
+        f'{output_path}/{img_name}.{output_format}',
+        format=output_format
+    )
+
+
+def read_img(image: str = None):
+    if image:
+        with open(image, 'rb') as img_file:
+            return img_file.read()
+    else:
+        raise Exception('No image provided')
+
+
+files_list = glob.glob('/Users/brito/Documents/Mestrado/Dataset/Odonto/*.HEIC')
+for _file in files_list:
+    convert_image(
+        _file, 'tiff', '/Users/brito/Documents/Mestrado/Dataset/Odonto/tiff/')
