a7f855c89b7c3dbfa300064ef0b90df801a0d9ea
Lucas Brito <lucasbsilva29@gmail.com>
2022-07-05 21:50:55 -0300

    feat: applying progress bar

--- images_converter.py
+++ images_converter.py
@@ -2,7 +2,9 @@ import io
 import glob
 import pyheif
 import whatimage
+
 from PIL import Image
+from tqdm import tqdm
 
 
 def convert_image(image: str = None, output_format: str = 'tiff', output_path: str = None):
@@ -35,10 +37,10 @@ def read_img(image: str = None):
         raise Exception('No image provided')
 
 
-files_list = glob.glob('/Users/brito/Documents/Mestrado/Dataset/Odonto/*.HEIC')
-for _file in files_list:
+files_list = glob.glob('Odonto/*.HEIC')
+for _file in tqdm(files_list):
     convert_image(
         _file,
         'png',
-        '/Users/brito/Documents/Mestrado/Dataset/Odonto/png/'
+        'Odonto/png/'
     )
